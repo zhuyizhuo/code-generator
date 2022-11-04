34a2e4674174c8d4fa85293d510c416ae3b17f28
zhuyizhuo <zhu.yizhuo@163.com>
2022-11-04 14:34:16 +0800

    控制台日志地址更新
    控制台日志地址更新

--- code-generator/src/main/java/com/github/zhuyizhuo/generator/constants/LinkConstants.java
+++ code-generator/src/main/java/com/github/zhuyizhuo/generator/constants/LinkConstants.java
@@ -12,13 +12,13 @@ public class LinkConstants {
     /** github 地址 */
     public static final String GITHUB_URL = "https://github.com/zhuyizhuo/code-generator/";
     /** 文档路径 */
-    public static final String DOC_URL = "http://zhuyizhuo.online/code-generator-doc/\nGithub: " + GITHUB_URL;
+    public static final String DOC_URL = "http://zhuyizhuo.github.io/code-generator-doc/\nGithub: " + GITHUB_URL;
     /** 常见问题 */
-    public static final String FAQ_DOC_URL = "http://zhuyizhuo.online/code-generator-doc/guide/faq.html";
+    public static final String FAQ_DOC_URL = "http://zhuyizhuo.github.io/code-generator-doc/guide/faq.html";
     /** 快速开始文档路径 */
-    public static final String QUICKSTART_DOC_URL = "http://zhuyizhuo.online/code-generator-doc/guide/quickstart.html";
+    public static final String QUICKSTART_DOC_URL = "http://zhuyizhuo.github.io/code-generator-doc/guide/quickstart.html";
     /** 生成器扩展文档路径 */
-    public static final String EXTENSION_DOC_BASE_URL = "http://zhuyizhuo.online/code-generator-doc/guide/extension.html";
+    public static final String EXTENSION_DOC_BASE_URL = "http://zhuyizhuo.github.io/code-generator-doc/guide/extension.html";
     public static final String EXTENSION_DOC_URL_A = EXTENSION_DOC_BASE_URL + "#新增或修改数据库类型和 Java 类型映射";
     public static final String EXTENSION_DOC_URL_B = EXTENSION_DOC_BASE_URL + "#新增或修改数据库类型和 Mybatis JdbcType 映射";
     /** Maven 仓库地址 */
